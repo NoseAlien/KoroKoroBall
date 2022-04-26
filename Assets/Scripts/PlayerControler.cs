02c4c0d0578a1ddc67e4917d68d9cc43b02d3ed8
KX _net <kx.net.com@gmail.com>
2022-04-26 16:10:32 +0900

    2022年4月26日

--- Assets/Scripts/PlayerControler.cs
+++ Assets/Scripts/PlayerControler.cs
@@ -6,11 +6,12 @@ public class PlayerControler : MonoBehaviour
 {
     public float power = 10;
     public Rigidbody rigidbody;
+    public Vector3 respawnPoint;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        respawnPoint = transform.position;
     }
 
     // Update is called once per frame
@@ -32,6 +33,25 @@ public class PlayerControler : MonoBehaviour
         if (Input.GetKey(KeyCode.LeftArrow))
         {
             rigidbody.AddForce(new Vector3(-1, 0, 0) * power);
+        }
+
+        if (Input.GetKey(KeyCode.Return) || transform.position.y <= -10)
+        {
+            Respawn();
+        }
+    }
+
+    public void Respawn()
+    {
+        transform.position = respawnPoint;
+        rigidbody.velocity = Vector3.zero;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "CheckPoint")
+        {
+            respawnPoint = other.transform.position;
         }
     }
 }
